a7cfc690409a1eb31d0fea2fc8001f80d2593174
ObieWalker <obinnawalker@gmail.com>
2018-11-24 07:36:05 +0100

    add not found page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,15 +12,15 @@ class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <Switch>
-          <div className="container">
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route path="/cheats" component={RequireAuthentication(LandingPage)} />
-            <Route path="/*" component={NotFound} />
-          </div>
-        </Switch>
+        <div className="container">
+          <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route path="/cheats" component={RequireAuthentication(LandingPage)} />
+              <Route component={NotFound} />
+          </Switch>
+        </div>
       </Router>
     );
   }
